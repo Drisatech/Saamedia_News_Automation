56b1a136ce00332a21df15e0d6e5741ae29c2a51
Aliyu Idris <drisatech@gmail.com>
2026-06-14 10:46:49 +0100

    Update WP credentials to use environment variables

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # 🌐 WordPress REST-URL Settings
 WORDPRESS_REST_URL = "https://saamedia.com.ng/wp-json/wp/v2"
-WP_USERNAME = "SAAMEDIA.INFO"
-WP_APP_PASSWORD = "WP_APP_PASSWORD"
+WP_USERNAME = os.getenv("WP_USERNAME")
+WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
 
 # 📞 WhatsApp Alert Config via CallMeBot
 WHATSAPP_API = "https://api.callmebot.com/whatsapp.php?phone=2348060466910&text=This+is+a+test&apikey=6321397"
